d7ea8221432f0dcb2c5cc36ed44651baec24e71c
Chao Ma <ynyounuo@me.com>
2017-11-07 17:08:09 -0800

    Fix autoshrink for the description

--- iQuiz/QuizTableViewController.swift
+++ iQuiz/QuizTableViewController.swift
@@ -9,13 +9,9 @@
 import UIKit
 
 class QuizTableViewController: UITableViewController {
-    @IBAction func btnSettingPressed(_ sender: Any) {
-        let alert = UIAlertController(title: "Settings go here", message: "", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-        self.present(alert, animated: true, completion: nil)
-    }
     
     var quizzes = [Quiz]()
+
     
     private func loadQuizzes() {
         let scienceImg = UIImage(named: "Science")
@@ -27,7 +23,7 @@ class QuizTableViewController: UITableViewController {
         guard let math = Quiz(name: "Mathematics", photo: mathImg, desc: "Questions about math") else {
             fatalError("Unable to instantiate math")
         }
-        guard let marvel = Quiz(name: "Marvel Super Heroes", photo: marvelImg, desc: "Questions about marvel super hero") else {
+        guard let marvel = Quiz(name: "Marvel Super Heroes", photo: marvelImg, desc: "Questions about marvel super heroes") else {
             fatalError("Unable to instantiate marvel")
         }
         quizzes += [science, math, marvel]
@@ -36,27 +32,17 @@ class QuizTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         loadQuizzes()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-    // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return quizzes.count
     }
 
@@ -68,7 +54,9 @@ class QuizTableViewController: UITableViewController {
             fatalError("The dequeued cell is not an instance of QuizCell.")
         }
         
-        // Fetches the appropriate meal for the data source layout.
+        cell.titleLabel.adjustsFontSizeToFitWidth=true
+        cell.desLabel.adjustsFontSizeToFitWidth=true
+        
         let quiz = quizzes[indexPath.row]
         
         cell.titleLabel.text = quiz.name
@@ -76,51 +64,4 @@ class QuizTableViewController: UITableViewController {
         cell.desLabel.text = quiz.desc
         return cell
     }
-    
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
