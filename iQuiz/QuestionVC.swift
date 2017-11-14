e0a0519a2bf3d925397eeb1db40dda3ac67ebb0e
Chao Ma <ynyounuo@me.com>
2017-11-14 04:36:55 -0800

    Add shadow to the buttons

--- iQuiz/QuestionVC.swift
+++ iQuiz/QuestionVC.swift
@@ -29,7 +29,7 @@ class QuestionVC: UIViewController {
     var qIndex:Int = 0
     var selectedAns:String = ""
     
-    let qDict: [[[String]]] = [
+    let qArr: [[[String]]] = [
         [["Electromagnetic radiation emitted from a nucleus is most likely to be in the form of",
           "gamma rays", "microwaves", "visible light", "ultraviolet radiation"],
          ["What is the limiting high-temperature molar heat capacity at constant volume of a gas-phase diatomic molecule?",
@@ -49,7 +49,8 @@ class QuestionVC: UIViewController {
          ["An off-hand comment by Stan Lee caused Iron Man to receive what odd addition to his armor?",
           "Iron Nose", "Iron Toes", "Iron Fingernails", "Iron Nipples"]]
     ]
-    
+
+
     var aRand: [Int] = [1, 2, 3, 4].shuffled()
     var qRand: [Int] = []
     
@@ -65,9 +66,9 @@ class QuestionVC: UIViewController {
             let avc = storyboard?.instantiateViewController(withIdentifier: "avc") as? AnswerVC
             avc?.index = self.index
             avc?.qIndex = self.qIndex
-            avc?.totalNum = self.qDict[index].count
+            avc?.totalNum = self.qArr[index].count
             avc?.correct = self.correct
-            avc?.correctAns = self.qDict[index][qRand[qIndex]][1]
+            avc?.correctAns = self.qArr[index][qRand[qIndex]][1]
             avc?.correctNum = self.correctNum
             avc?.qRand = self.qRand
             self.presentL(avc!)
@@ -80,14 +81,21 @@ class QuestionVC: UIViewController {
     
     @IBAction func redoPressed(_ sender: UIButton) {
         submited = false
-        redoBtn.isEnabled = false
+        correct = false
+        correctNum -= 1
+        sender.layer.shadowOffset = CGSize(width: 1.5, height: 1.5)
+        sender.isEnabled = false
         submitBtn.isEnabled = true
+        submitBtn.layer.shadowOffset = CGSize(width: 3.0, height: 3.0)
+        for item in answerBtns! {
+            item.isEnabled = true
+        }
     }
     
     @IBAction func answerPressed(_ sender: UIButton) {
         sender.isSelected = true
-        correct = false
         selectedAns = sender.title(for: .normal)!
+        correct = false
         selected = true
         for item in answerBtns! {
             if item.tag != sender.tag {
@@ -96,9 +104,11 @@ class QuestionVC: UIViewController {
             if item.isSelected {
                 item.backgroundColor = self.view.tintColor
                 item.setTitleColor(.white, for: .normal)
+                item.layer.shadowOffset = CGSize(width: 1.5, height: 1.5)
             } else {
                 item.backgroundColor = .white
                 item.setTitleColor(self.view.tintColor, for: .normal)
+                item.layer.shadowOffset = CGSize(width: 3.0, height: 3.0)
             }
         }
     }
@@ -107,10 +117,15 @@ class QuestionVC: UIViewController {
         if (selected) {
             submited = true
             sender.isEnabled = false
+            sender.layer.shadowOffset = CGSize(width: 1.5, height: 1.5)
             redoBtn.isEnabled = true
-            if (selectedAns == qDict[index][qRand[qIndex]][1]) {
+            redoBtn.layer.shadowOffset = CGSize(width: 3.0, height: 3.0)
+            if (selectedAns == qArr[index][qRand[qIndex]][1]) {
                 correct = true
                 correctNum += 1
+            }
+            for item in answerBtns! {
+                item.isEnabled = false
             }
         } else {
             let alert = UIAlertController(title: "No answer selected!", message: "", preferredStyle: .alert)
@@ -122,13 +137,18 @@ class QuestionVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         if (qRand.count == 0) {
-            qRand = Array(0...qDict[index].count - 1).shuffled()
+            qRand = Array(0...qArr[index].count - 1).shuffled()
         }
         load(qIndex)
-        toolbarTitle.text = toolbarTitle.text! + " (\(qIndex + 1)/\(qDict[index].count))"
+        toolbarTitle.text = toolbarTitle.text! + " (\(qIndex + 1)/\(qArr[index].count))"
         for button in buttons! {
             button.layer.cornerRadius = 5
             button.layer.borderWidth = 1
+            button.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.25).cgColor
+            button.layer.shadowOffset = CGSize(width: 3.0, height: 3.0)
+            button.layer.shadowOpacity = 0.5
+            button.layer.shadowRadius = 0.0
+            button.layer.masksToBounds = false
             button.contentEdgeInsets = UIEdgeInsetsMake(5, 10, 5, 10)
             button.titleLabel?.adjustsFontSizeToFitWidth = true
             button.titleLabel?.lineBreakMode = .byClipping
@@ -144,8 +164,8 @@ class QuestionVC: UIViewController {
         case 1: toolbarTitle.text = "Mathematics"
         case 2: toolbarTitle.text = "Marvel Super Heroes"
         default: break }
-        question.text = qDict[index][qRand[qIndex]][0]
-        let answers = qDict[index][qRand[qIndex]]
+        question.text = qArr[index][qRand[qIndex]][0]
+        let answers = qArr[index][qRand[qIndex]]
         a.setTitle(answers[aRand[0]], for: .normal)
         b.setTitle(answers[aRand[1]], for: .normal)
         c.setTitle(answers[aRand[2]], for: .normal)
