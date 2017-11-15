48f84a0b5a303668033781615cd51de8c92f0640
Chao Ma <ynyounuo@me.com>
2017-11-14 21:45:46 -0800

    Add popover view

---
+++ iQuiz/SettingsVC.swift
@@ -0,0 +1,38 @@
+//
+//  SettingsVC.swift
+//  iQuiz
+//
+//  Created by ​ on 11/14/17.
+//  Copyright © 2017 iGuest. All rights reserved.
+//
+
+import UIKit
+
+class SettingsVC: UIViewController {
+
+    @IBAction func dismiss(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
